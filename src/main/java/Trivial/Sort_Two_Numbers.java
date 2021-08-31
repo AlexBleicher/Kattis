ffce7d8b04cc92ac53e8d19faa7acb932d7edea8
AlexBleicher <alexander.bleicher@gmail.com>
2021-08-31 14:41:30 +0200

    Sort_Two_Numbers

---
+++ src/main/java/Trivial/Sort_Two_Numbers.java
@@ -0,0 +1,20 @@
+package Trivial;
+
+import java.util.Scanner;
+
+public class Sort_Two_Numbers {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if(a<=b){
+            System.out.println(a + " " + b);
+        } else{
+            System.out.println(b + " " + a);
+        }
+
+    }
+}
