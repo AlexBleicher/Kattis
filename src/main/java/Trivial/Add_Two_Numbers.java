73f91fe8b58026241c1a99d132b411316425a5bc
AlexBleicher <alexander.bleicher@gmail.com>
2021-09-03 08:27:00 +0200

    Add_Two_Numbers

---
+++ src/main/java/Trivial/Add_Two_Numbers.java
@@ -0,0 +1,15 @@
+package Trivial;
+
+import java.util.Scanner;
+
+public class Add_Two_Numbers {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a + b);
+    }
+}
